f243c039e047f0b9d1b3eb1a5824096717ce31a1
lucaslukero <lucas.salazar.72@gmail.com>
2016-10-20 23:48:53 -0300

    Now default behavior is converting pdf to images, and then creating the HTML with them. URL can be changed now. Updated call.py. Updated README

--- call.py
+++ call.py
@@ -3,7 +3,11 @@
 from pdf_to_html_json import pdf_to_html_json
 import sys
 
-if len(sys.argv) > 2:
+if len(sys.argv) >= 5:
+    pdf_to_html_json(sys.argv[1], sys.argv[2], int(sys.argv[3]), sys.argv[4])
+elif len(sys.argv) == 4:
+    pdf_to_html_json(sys.argv[1], sys.argv[2], int(sys.argv[3]))
+elif len(sys.argv) == 3:
     pdf_to_html_json(sys.argv[1], sys.argv[2])
 elif len(sys.argv) == 2:
     pdf_to_html_json(sys.argv[1])
